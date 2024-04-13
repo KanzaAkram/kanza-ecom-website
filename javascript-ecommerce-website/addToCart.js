0c5846aae63ace5297220791f7c787872af0750a
Kanza Akram <kanzaakram123@gmail.com>
2024-04-13 16:57:28 +0500

    Fix bug in addToCart function

--- javascript-ecommerce-website/addToCart.js
+++ javascript-ecommerce-website/addToCart.js
@@ -1,5 +1,5 @@
 import { getCartProductFromLS } from "./getCartProducts";
-// import { showToast } from "./showToast";
+import { showToast } from "./showToast";
 import { updateCartValue } from "./updateCartValue";
 
 // -----------------------------------------------------
@@ -12,21 +12,23 @@ getCartProductFromLS();
 // to add the data into localStorage
 // --------------------------------------------------------
 export const addToCart = (event, id, stock) => {
-  let arrLocalStorageProduct = getCartProductFromLS(); //assuming when this function is called we already have some data in the localStorage so that we can update the cart value and also to get the data always ready from localStorage
+  let arrLocalStorageProduct = getCartProductFromLS();
 
   const currentProdElem = document.querySelector(`#card${id}`);
   let quantity = currentProdElem.querySelector(".productQuantity").innerText;
   let price = currentProdElem.querySelector(".productPrice").innerText;
   //   console.log(quantity, price);
-  price = price.replace("RS.", "");
+  price = price.replace("₹", "");
 
-  let existingProd = arrLocalStorageProduct.find((curProd) => curProd.id === id);
+  let existingProd = arrLocalStorageProduct.find(
+    (curProd) => curProd.id === id
+  );
+
   console.log(existingProd);
 
   if (existingProd && quantity > 1) {
     quantity = Number(existingProd.quantity) + Number(quantity);
     price = Number(price * quantity);
-    
     let updatedCart = { id, quantity, price };
 
     updatedCart = arrLocalStorageProduct.map((curProd) => {
@@ -40,6 +42,7 @@ export const addToCart = (event, id, stock) => {
   }
 
   if (existingProd) {
+    // alert("bhai duplicate hai");
     return false;
   }
 
@@ -47,7 +50,7 @@ export const addToCart = (event, id, stock) => {
   quantity = Number(quantity);
 
   arrLocalStorageProduct.push({ id, quantity, price });
-  localStorage.setItem("cartProductLS", JSON.stringify(arrLocalStorageProduct));     //add the data into localStorage in form of string that is why i have converted it into string
+  localStorage.setItem("cartProductLS", JSON.stringify(arrLocalStorageProduct));
 
   //update the cart button value
   updateCartValue(arrLocalStorageProduct);
